e3e69cc3f81902b3acbc64398b71c9913e0d6416
Sam Lam <xlin67948gmail.com>
2024-09-06 12:43:19 +0800

    the basic course is done

---
+++ JS/test.js
@@ -0,0 +1,16 @@
+const name = 'Sam';
+
+const first = () => {
+    let a = 1;
+    const b = second(7, 9);
+    a = a + b;
+    return a;
+}
+
+function second (x, y) {
+    var c = 2;
+    return c;
+}
+
+const x = first();
+console.log(x);
